f4df5779d9c5538fef31f4cb444c654bb4ff82e2
rebecca njeri <rebecca.njeri@student.moringaschool.com>
2022-10-26 21:49:05 +0300

    DONE

---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+class Person
+    def talk 
+        puts  "Hello World!"
+    end
+    def walk
+        puts "The Person is walking"
+    end
+end
+snoopy = Person.new
+snoopy.talk
+snoopy = Person.new
+snoopy.walk
